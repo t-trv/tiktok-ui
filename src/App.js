bda24faf4944c3fb12dc2c46d7febcf65b0d125a
trvv <trhuvan@gmail.com>
2025-09-10 23:02:24 +0700

    Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { Fragment } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from './components/Layout';
+
 function App() {
   return (
-    <div className="App">
-      <h1>ReactJS</h1>
-      <p>Xin chao moi nguoi</p>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        // Xu ly route cho app
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component;
+
+            // Logic xu ly layout
+            let Layout = DefaultLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
